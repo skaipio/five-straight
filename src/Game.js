c77cfe867e0d8746cb498ec58b729c8e3dd719b8
skaipio <senni.kaipio@gmail.com>
2017-04-09 19:55:42 +0300

    Fix bug where straights were not considered a victory if piece was set in middle of it

--- src/Game.js
+++ src/Game.js
@@ -39,21 +39,50 @@ const hasStraight = (board, boardSize, index, player, increment) => {
 const getEvaluator = (board, boardSize, lastMove, player) =>
   (increment) => hasStraight(board, boardSize, lastMove, player, increment);
 
+// increment determines ray direction
+const getScoreFromRay = (board, boardSize, index, player, increment) => {
+  const startingPoint = getPoint(boardSize, index);
+
+  const isInBounds = (x, y) => (
+    _.inRange(x, boardSize) && _.inRange(y, boardSize));
+
+  const pointHasPlayer = (x, y) => {
+    if (isInBounds(x, y, boardSize)) {
+      const index = indexAt(boardSize, x, y);
+      return board[index] === player;
+    }
+    return false;
+  }
+
+  let playerScore = 0;
+
+  for (var x = startingPoint.x, y = startingPoint.y; pointHasPlayer(x, y); x += increment.x, y += increment.y) {
+    playerScore++;
+  }
+
+  return playerScore;
+}
+
 class Game {
   /**
   Checks every direction from indexToCheck to see if there's a straight of REQUIRED_LENGTH.
   Returns true if there is, otherwise false.
   **/
   static hasStraightAtIndex(board, boardSize, indexToCheck, player) {
-    const evaluator = getEvaluator(board, boardSize, indexToCheck, player);
-    let straight = evaluator({x: 1, y: 0});
-    straight = straight || evaluator({x: -1, y: 0});
-    straight = straight || evaluator({x: 0, y: 1});
-    straight = straight || evaluator({x: 0, y: -1});
-    straight = straight || evaluator({x: 1, y: 1});
-    straight = straight || evaluator({x: -1, y: 1});
-    straight = straight || evaluator({x: 1, y: -1});
-    straight = straight || evaluator({x: -1, y: -1});
+    const hasFive = (increment) => {
+      const oppositeIncrement = {
+        x: -increment.x,
+        y: -increment.y
+      };
+      const score = getScoreFromRay(board, boardSize, indexToCheck, player, increment);
+      const oppositeScore = getScoreFromRay(board, boardSize, indexToCheck, player, oppositeIncrement);
+      // -1 because getScoreFromRay adds starting point as well
+      return score + oppositeScore - 1 >= REQUIRED_LENGTH;
+    }
+    let straight = hasFive({x: 1, y: 0});
+    straight = straight || hasFive({x: 0, y: 1});
+    straight = straight || hasFive({x: 1, y: 1});
+    straight = straight || hasFive({x: -1, y: 1});
     return straight;
   }
 }
